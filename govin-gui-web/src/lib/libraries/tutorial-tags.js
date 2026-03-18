35ad5a8e3f63ba4e01f0d70740a31022ccee8120
mangeshgawas <manikgawas07@gmail.com>
2026-03-18 12:30:43 +0530

    Add govin-gui-webrecent folder

---
+++ govin-gui-web/src/lib/libraries/tutorial-tags.js
@@ -0,0 +1,11 @@
+import messages from './tag-messages.js';
+export default [
+    // {tag: 'animation', intlLabel: messages.animation},
+    // {tag: 'art', intlLabel: messages.art},
+    // {tag: 'music', intlLabel: messages.music},
+    // {tag: 'games', intlLabel: messages.games},
+    // {tag: 'stories', intlLabel: messages.stories},
+    {tag: 'govin', intlLabel: messages.govin}, //Tag for Govin
+    {tag: 'scratch', intlLabel: messages.scratch}, //Tag for Scratch
+    {tag: 'hardware', intlLabel: messages.hardware}, //Tag for Hardware
+];
